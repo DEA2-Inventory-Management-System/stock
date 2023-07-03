c45c1cf5f0ca4940970875eb7e18722a9cebffc4
Ashinka-X <dilshanashinka98@gmail.com>
2023-07-03 15:16:29 +0530

    Spring boot set-up

---
+++ Stock/Stock_Page/src/main/java/com/Inventory/Management/System/Stock_Page/StockPageApplication.java
@@ -0,0 +1,13 @@
+package com.Inventory.Management.System.Stock_Page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPageApplication.class, args);
+	}
+
+}
---
+++ Stock/Stock_Page/src/test/java/com/Inventory/Management/System/Stock_Page/StockPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Inventory.Management.System.Stock_Page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
